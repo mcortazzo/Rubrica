6fa122ee5468fa63535a3d7475caa33c8a7b842d
mcortazzo <mariapia.cortazzo@gmail.com>
2020-05-03 19:14:19 +0200

    configuration per security

---
+++ SuperRubricaTelefonica/src/main/java/com/devioooh/srt/configuration/MvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.devioooh.srt.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
